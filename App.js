20520c0ab7f318ffc8e2a0c6ce83d3469e1ee781
alessandro marrone <alessandro.marrone.86@gmail.com>
2021-08-27 20:09:03 +0100

    Defined SearchBox component

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ const navigator = createStackNavigator({
 }, {
     initialRouteName: 'Home',
     defaultNavigationOptions: {
-        title: 'Home'
+        title: 'Phorest Salon'
     }
 })
 
---
+++ src/components/SearchBox.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import {View, StyleSheet, TextInput} from "react-native"
+// import {Feather} from '@expo/vector-icons'
+
+const SearchBox = ({email, onEmailChange}) => {
+
+    return <View style={styles.backgroundStyle}>
+        {/*<Feather name="search" style={styles.iconStyle}/>*/}
+        <TextInput autoCorrect={false}
+                   autoCapitalize="none"
+                   placeholder="Email"
+                   style={styles.inputStyle}
+                   value={email} onChangeText={onEmailChange}
+        />
+    </View>
+}
+
+const styles = StyleSheet.create({
+    backgroundStyle: {
+        backgroundColor: '#EEEEEE',
+        height: 50,
+        borderRadius: 5,
+        marginHorizontal: 15, marginTop: 10,
+        flexDirection: 'row'
+    },
+    inputStyle: {
+        flex: 1,
+        fontSize: 18
+    },
+    iconStyle: {
+        marginHorizontal: 15,
+        fontSize: 35,
+        alignSelf: 'center'
+    }
+})
+
+export default SearchBox
